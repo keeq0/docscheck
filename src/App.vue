96b24c1c833d1f69687b49437e2f6c21b90c6fd1
keeq0 <myakotnykh1689@gmail.com>
2025-03-28 15:17:45 +0300

    created application and service page

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <ServicePage />
+</template>
+
+<script>
+import ServicePage from './views/ServicePage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ServicePage
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
