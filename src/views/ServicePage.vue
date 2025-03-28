0d1323636a447cea86d39c8cadd744a35e2b72b7
keeq0 <myakotnykh1689@gmail.com>
2025-03-28 18:25:06 +0300

    added content layout component

--- src/views/ServicePage.vue
+++ src/views/ServicePage.vue
@@ -1,15 +1,17 @@
 <template>
-    <ServiceHeader />
-</template>
-<script>
-import ServiceHeader from '@/components/ServiceHeader.vue';
-
-export default {
-    components: {
-        ServiceHeader
-    }
-}
-</script>
-<style>
-    
-</style>
+    <div>
+      <ServiceHeader />
+      <ServiceLayout />
+    </div>
+  </template>
+  
+  <script>
+  import ServiceHeader from '@/components/ServiceHeader.vue'
+  import ServiceLayout from '@/components/ServiceLayout.vue'
+  
+  export default {
+    name: 'ServicePage',
+    components: { ServiceHeader, ServiceLayout }
+  }
+  </script>
+  
