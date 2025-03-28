177f50c6779f0b56e98ea3a9ef4cedc96798887f
keeq0 <myakotnykh1689@gmail.com>
2025-03-28 19:41:34 +0300

    content component

---
+++ src/components/ServiceLayout.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="layout">
+      <NavigationPanel/>
+    </div>
+  </template>
+  
+  <script>
+  import NavigationPanel from './NavigationPanel.vue'
+  
+  export default {
+    components: {
+        NavigationPanel,
+     },
+  }
+  </script>
+  
+  <style scoped>
+  .layout {
+    margin: 0 auto;
+    display: flex;
+    width: 1200px;
+    padding-top: 50px;
+  }
+  </style>
+  
