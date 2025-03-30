e04187965bbe7187f1e0ca91b10e98d0c3ad0cb3
keeq0 <myakotnykh1689@gmail.com>
2025-03-30 10:32:54 +0300

    add footer component

--- src/views/ServicePage.vue
+++ src/views/ServicePage.vue
@@ -2,15 +2,20 @@
   <div>
     <ServiceHeader />
     <ServiceLayout />
+    <ServiceFooter />
   </div>
 </template>
 
 <script>
+import ServiceFooter from '@/components/ServiceFooter.vue';
 import ServiceHeader from '@/components/ServiceHeader.vue'
 import ServiceLayout from '@/components/ServiceLayout.vue'
 
 export default {
   name: 'ServicePage',
-  components: { ServiceHeader, ServiceLayout }
+  components: { 
+    ServiceHeader,
+    ServiceLayout,
+    ServiceFooter }
 }
 </script>
